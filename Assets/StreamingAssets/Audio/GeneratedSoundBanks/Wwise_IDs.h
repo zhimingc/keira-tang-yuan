09cf9552214e58ad03e4ba42c13948c1d4bcab69
Gwen <gwen@imbainteractive.com>
2021-11-17 00:21:31 +0800

    Added more VOs, SFX
    1st draft of all the interactable SFX
    VOs for deliveryman, paul, alex, keira, keira's mum

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,105 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID SFX_00GEN_CLICK = 1010927348U;
+        static const AkUniqueID SFX_00GEN_DOOR = 4280253252U;
+        static const AkUniqueID SFX_00GEN_DOORBELL = 2261318223U;
+        static const AkUniqueID SFX_01OPN_COUCH = 2772396626U;
+        static const AkUniqueID SFX_01OPN_JET = 1854270925U;
+        static const AkUniqueID SFX_01OPN_PHOTO = 376518116U;
+        static const AkUniqueID SFX_02DLV_BAG = 3774266052U;
+        static const AkUniqueID SFX_02DLV_FOOTSTEPS = 2457604163U;
+        static const AkUniqueID SFX_03ZOM_ZOOM = 2902341918U;
+        static const AkUniqueID SFX_03ZOM_ZOOM_STOP = 2852847869U;
+        static const AkUniqueID SFX_04ACM_AC = 3619859189U;
+        static const AkUniqueID SFX_04ACM_FIX = 2256124074U;
+        static const AkUniqueID SFX_04ACM_LIGHTER = 41232090U;
+        static const AkUniqueID SFX_04ACM_POUR = 2950033031U;
+        static const AkUniqueID SFX_05MIR_SELFIE = 2614593559U;
+        static const AkUniqueID SFX_06INS_OPENBOOK = 1297885619U;
+        static const AkUniqueID SFX_08CHF_CUTLERY = 2038536189U;
+        static const AkUniqueID SFX_09HRW_KTV = 2627611537U;
+        static const AkUniqueID SFX_09HRW_PACK = 2319967843U;
+        static const AkUniqueID SFX_10VCM_PHONE = 912741289U;
+        static const AkUniqueID SFX_10VCM_RECLINER = 1654728357U;
+        static const AkUniqueID SFX_10VCM_VACUUM_START = 2224723115U;
+        static const AkUniqueID SFX_10VCM_VACUUM_STOP = 839339977U;
+        static const AkUniqueID SFX_11KEI_DOG = 2342739123U;
+        static const AkUniqueID SFX_11KEI_DOORBELL = 1269518332U;
+        static const AkUniqueID VO_02DLV_010_DELIVERY = 3273819246U;
+        static const AkUniqueID VO_02DLV_020_DELIVERY = 2655851741U;
+        static const AkUniqueID VO_03ZOM_010_WENDY = 3791463254U;
+        static const AkUniqueID VO_03ZOM_040_WENDY = 2981718853U;
+        static const AkUniqueID VO_03ZOM_060_WENDY = 1131043679U;
+        static const AkUniqueID VO_03ZOM_080_WENDY = 3875258401U;
+        static const AkUniqueID VO_03ZOM_090_PAUL = 1433897677U;
+        static const AkUniqueID VO_04ACM_010_ALEX = 2867537081U;
+        static const AkUniqueID VO_04ACM_030_ALEX = 1761702355U;
+        static const AkUniqueID VO_04ACM_060_ALEX = 333498414U;
+        static const AkUniqueID VO_04ACM_080_ALEX = 1937623344U;
+        static const AkUniqueID VO_06INS_030_MERWYN = 410205544U;
+        static const AkUniqueID VO_06INS_050_MERWYN = 1285751974U;
+        static const AkUniqueID VO_06INS_060_MERWYN = 4001995393U;
+        static const AkUniqueID VO_06INS_070_MERWYN = 3094539428U;
+        static const AkUniqueID VO_06INS_100_MERWYN = 3573006388U;
+        static const AkUniqueID VO_08CHF_010_ETHAN = 831783015U;
+        static const AkUniqueID VO_08CHF_020_ETHAN = 333640230U;
+        static const AkUniqueID VO_08CHF_050_ETHAN = 3568377795U;
+        static const AkUniqueID VO_08CHF_070_ETHAN = 1935602725U;
+        static const AkUniqueID VO_08CHF_080_ETHAN = 815836500U;
+        static const AkUniqueID VO_08CHF_090_ETHAN = 2932602975U;
+        static const AkUniqueID VO_08CHF_110_ETHAN = 513475766U;
+        static const AkUniqueID VO_08CHF_120_ETHAN = 296267319U;
+        static const AkUniqueID VO_08CHF_130_ETHAN = 903915116U;
+        static const AkUniqueID VO_09HRW_020_HUSBAND = 2240928088U;
+        static const AkUniqueID VO_11KEI_010_JANNAH = 2816861167U;
+        static const AkUniqueID VO_11KEI_020_JANNAH = 3576593044U;
+        static const AkUniqueID VO_11KEI_030_JANNAH = 2776257953U;
+        static const AkUniqueID VO_11KEI_040_KEIRA = 3555550800U;
+        static const AkUniqueID VO_11KEI_050_KEIRA = 356528211U;
+        static const AkUniqueID VO_11KEI_060_KEIRA = 4016277978U;
+        static const AkUniqueID VO_11KEI_070_KEIRA = 1361983429U;
+        static const AkUniqueID VO_11KEI_080_KEIRA = 3579149732U;
+        static const AkUniqueID VO_11KEI_100_KEIRA = 942777237U;
+        static const AkUniqueID VO_11KEI_110_MUM = 3523608175U;
+        static const AkUniqueID VO_11KEI_120_MUM = 1310102706U;
+        static const AkUniqueID VO_11KEI_160_HUSBAND = 2569653828U;
+        static const AkUniqueID VO_11KEI_170_HUSBAND = 3103793179U;
+        static const AkUniqueID VO_11KEI_180_HUSBAND = 3250207694U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN_BANK = 3989171651U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID BGM = 412724365U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID VO = 1534528548U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
